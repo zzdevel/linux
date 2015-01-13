becd11244f635804e0dda34d79f15ba31ed4be2a
Morten Rasmussen <morten.rasmussen@arm.com>
2015-01-13 13:50:46 +0000

    sched: Introduce SD_SHARE_CAP_STATES sched_domain flag
    cpufreq is currently keeping it a secret which cpus are sharing
    clock source. The scheduler needs to know about clock domains as well
    to become more energy aware. The SD_SHARE_CAP_STATES domain flag
    indicates whether cpus belonging to the sched_domain share capacity
    states (P-states).
    
    There is no connection with cpufreq (yet). The flag must be set by
    the arch specific topology code.
    
    cc: Russell King <linux@arm.linux.org.uk>
    cc: Ingo Molnar <mingo@redhat.com>
    cc: Peter Zijlstra <peterz@infradead.org>
    
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5773,7 +5773,8 @@ static int sd_degenerate(struct sched_domain *sd)
 			 SD_SHARE_CPUCAPACITY |
 			 SD_ASYM_CPUCAPACITY |
 			 SD_SHARE_PKG_RESOURCES |
-			 SD_SHARE_POWERDOMAIN)) {
+			 SD_SHARE_POWERDOMAIN |
+			 SD_SHARE_CAP_STATES)) {
 		if (sd->groups != sd->groups->next)
 			return 0;
 	}
@@ -5806,7 +5807,8 @@ sd_parent_degenerate(struct sched_domain *sd, struct sched_domain *parent)
 				SD_SHARE_CPUCAPACITY |
 				SD_SHARE_PKG_RESOURCES |
 				SD_PREFER_SIBLING |
-				SD_SHARE_POWERDOMAIN);
+				SD_SHARE_POWERDOMAIN |
+				SD_SHARE_CAP_STATES);
 		if (parent->groups->sge) {
 			parent->flags &= ~SD_LOAD_BALANCE;
 			return 0;
@@ -6516,6 +6518,7 @@ static int sched_domains_curr_level;
  *   SD_NUMA                - describes NUMA topologies
  *   SD_SHARE_POWERDOMAIN   - describes shared power domain
  *   SD_ASYM_CPUCAPACITY    - describes mixed capacity topologies
+ *   SD_SHARE_CAP_STATES    - describes shared capacity states
  *
  * Odd one out, which beside describing the topology has a quirk also
  * prescribes the desired behaviour that goes along with it:
@@ -6528,7 +6531,8 @@ static int sched_domains_curr_level;
 	 SD_NUMA |			\
 	 SD_ASYM_PACKING |		\
 	 SD_ASYM_CPUCAPACITY |		\
-	 SD_SHARE_POWERDOMAIN)
+	 SD_SHARE_POWERDOMAIN |		\
+	 SD_SHARE_CAP_STATES)
 
 static struct sched_domain *
 sd_init(struct sched_domain_topology_level *tl,
