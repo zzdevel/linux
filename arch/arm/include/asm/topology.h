39b4ab485bd0f02de5221a266f61cfbd28bfad18
Morten Rasmussen <morten.rasmussen@arm.com>
2015-04-14 16:25:31 +0100

    arm: Re-wire cpu-invariant load-tracking support up to the scheduler
    Since commit 8cd5601c5060 ("sched/fair: Convert arch_scale_cpu_capacity()
    from weak function to #define") the wiring has to be done by associating
    arch_scale_cpu_capacity with the actual implementation provided by the
    arch.
    
    Define arch_scale_freq_capacity to use the arch-specific function
    scale_cpu_capacity().
    
    cc: Russell King <linux@arm.linux.org.uk>
    Signed-off-by: Morten Rasmussen <morten.rasmussen@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- arch/arm/include/asm/topology.h
+++ arch/arm/include/asm/topology.h
@@ -24,6 +24,10 @@ void init_cpu_topology(void);
 void store_cpu_topology(unsigned int cpuid);
 const struct cpumask *cpu_coregroup_mask(int cpu);
 
+struct sched_domain;
+#define arch_scale_cpu_capacity scale_cpu_capacity
+extern unsigned long scale_cpu_capacity(struct sched_domain *sd, int cpu);
+
 #else
 
 static inline void init_cpu_topology(void) { }
