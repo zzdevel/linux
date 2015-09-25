9bf3597c8dba18e196125af0036d37c1aca555d1
Dietmar Eggemann <dietmar.eggemann@arm.com>
2015-09-25 17:15:11 +0100

    arm64: Wire frequency-invariant load-tracking support up to the scheduler
    Define arch_scale_freq_capacity to use the arch-specific function
    scale_freq_capacity().
    
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Dietmar Eggemann <dietmar.eggemann@arm.com>

--- arch/arm64/include/asm/topology.h
+++ arch/arm64/include/asm/topology.h
@@ -36,6 +36,13 @@ struct sched_domain;
 #define arch_scale_cpu_capacity scale_cpu_capacity
 extern unsigned long scale_cpu_capacity(struct sched_domain *sd, int cpu);
 
+#ifdef CONFIG_CPU_FREQ
+
+#define arch_scale_freq_capacity scale_freq_capacity
+extern unsigned long scale_freq_capacity(struct sched_domain *sd, int cpu);
+
+#endif /* CONFIG_CPU_FREQ */
+
 #include <asm-generic/topology.h>
 
 #endif /* _ASM_ARM_TOPOLOGY_H */
